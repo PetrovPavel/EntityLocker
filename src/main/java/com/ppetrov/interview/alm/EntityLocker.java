4f558766702029254c3a193b4f985bc8b1659fa5
PetrovPavel <petrov.pavel.89@gmail.com>
2020-08-03 09:19:40 +0300

    Added java doc.

--- src/main/java/com/ppetrov/interview/alm/EntityLocker.java
+++ src/main/java/com/ppetrov/interview/alm/EntityLocker.java
@@ -6,11 +6,20 @@ import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+/**
+ * Allows to perform both ID-based locking and global locking for running a protected code.
+ * Locking is not shared between different instances of EntityLocker class.
+ * EntityLocker executes protected code in the calling thread, it does not perform any execution management.
+ */
 public final class EntityLocker<T extends IEntityID> {
 
     private final Map<T, ReentrantLock> locksByIDs = new ConcurrentHashMap<>();
     private final ReadWriteLock globalLock = new ReentrantReadWriteLock();
 
+    /**
+     * Runs globally locked protected code.
+     * If any ID-based locking code is running at the moment, call to globalLock waits for it to finish before starting.
+     */
     public final void globalLock(Runnable protectedCode) {
         globalLock.writeLock().lock();
         try {
@@ -20,6 +29,13 @@ public final class EntityLocker<T extends IEntityID> {
         }
     }
 
+    /**
+     * Runs protectedCode, locking it by id.
+     * At most one thread can execute protected code on that entity.
+     * If there’s a concurrent request to lock the same entity, the other thread waits until the entity becomes available.
+     * If the globalLock is acquired, lock-by-id request waits for it to finish its work.
+     * Allows reentrant locking.
+     */
     public final void lockAndRun(T id, Runnable protectedCode) {
         globalLock.readLock().lock();
         try {
@@ -45,6 +61,10 @@ public final class EntityLocker<T extends IEntityID> {
         }
     }
 
+    /**
+     * This method is supposed to be used only for testing and debugging purposes.
+     * @return true if the specified id is locked by any thread.
+     */
     public final boolean isLocked(T id) {
         ReentrantLock lock = locksByIDs.get(id);
         return lock != null && lock.isLocked();
--- src/main/java/com/ppetrov/interview/alm/IEntityID.java
+++ src/main/java/com/ppetrov/interview/alm/IEntityID.java
@@ -1,4 +1,10 @@
 package com.ppetrov.interview.alm;
 
+/**
+ * Instances of IEntityID are used as locking objects for EntityLocker.
+ *
+ * Implementations should consider to properly override equals and hashCode methods,
+ * because EntityLocker's implementation uses instances of this interface as keys in the map.
+ */
 public interface IEntityID {
 }
